94868e1536e55e1e907a149e1846910000ae4dbf
Jose Mora Loria <jdmora@backcountry.com>
2020-12-15 00:48:29 -0600

    Cambio a branch de BE.

--- vvuelos_frontend/src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  //baseURL: "http://vvuelosbackend.azurewebsites.net/api",
-  baseURL: "https://localhost:44332/api",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
