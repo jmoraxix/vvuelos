7beaa40a290c843b7141e3c066e5ebe23f3635f9
Jose Mora Loria <jdmora@backcountry.com>
2020-12-15 01:08:28 -0600

    Cambio titulo home.

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className="jumbotron">
-        Bienvenidos
+        Bienvenidos a vVuelos
       </div>
     );
   }
