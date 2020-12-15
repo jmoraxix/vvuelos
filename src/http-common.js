8010ec83399aaa4872c3fe267341bc494685d420
Jose Mora Loria <jdmora@backcountry.com>
2020-12-15 01:03:40 -0600

    Arreglo de la URL del backend.

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "http://vvuelosbackend.azurewebsites.net/api",
-  baseURL: "https://localhost:44332/api",
+  baseURL: "http://vvuelosbackend.azurewebsites.net/api",
+  //baseURL: "https://localhost:44332/api",
   headers: {
     "Content-type": "application/json"
   }
