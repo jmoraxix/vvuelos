c49e1c8743ab35566833384bf2d79de6db49274b
JSaenzCalvo <49600564+JSaenzCalvo@users.noreply.github.com>
2020-12-16 20:00:03 -0600

    RolUsuario

---
+++ Controllers/RolusuariosController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using vvuelos_backend;
+
+namespace vvuelos_backend.Controllers
+{
+    public class RolusuariosController : ApiController
+    {
+        private vvuelosEntities db = new vvuelosEntities();
+
+        // GET: api/Rolusuarios
+     
+        public IQueryable<Usuario> GetUsuarios()
+        {
+            
+            return db.Usuarios;
+        }
+
+        // POST: api/Rolusuarios
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult PostUsuario(string usuarioid, int rolid)
+        {
+            Usuario usuario = db.Usuarios.Where(Usuario => Usuario.UsuarioID.Equals(usuarioid)).FirstOrDefault();
+            Rol rol = db.Rols.Where(Rol => Rol.Codigo.Equals(rolid)).FirstOrDefault();
+
+            usuario.Rols.Add(rol);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (UsuarioExists(usuario.UsuarioID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = usuario.UsuarioID }, rol);
+        }
+
+        // DELETE: api/Rolusuarios/5
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult DeleteUsuario(string usuarioid, int rolid)
+        {
+            Usuario usuario = db.Usuarios.Where(Usuario => Usuario.UsuarioID.Equals(usuarioid)).FirstOrDefault();
+            Rol rol = db.Rols.Where(Rol => Rol.Codigo.Equals(rolid)).FirstOrDefault();
+            if (usuario == null && rol ==null)
+            {
+                return NotFound();
+            }
+
+            usuario.Rols.Remove(rol);
+            db.SaveChanges();
+
+            return Ok(usuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsuarioExists(string id)
+        {
+            return db.Usuarios.Count(e => e.UsuarioID == id) > 0;
+        }
+    }
+}
--- DBvvuelos.Designer.cs
+++ DBvvuelos.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Documents\GitHub\vvuelosfront\DBvvuelos.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Saenz\Documents\GitHub\vvuelosbackend\DBvvuelos.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
