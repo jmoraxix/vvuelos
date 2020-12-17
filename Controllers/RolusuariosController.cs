72f332191aa78a7c8083ef09ef9f107f250edae9
JSaenzCalvo <josesaenzcalvo@gmail.com>
2020-12-16 22:14:36 -0600

    Cors rolusuario

--- Controllers/RolusuariosController.cs
+++ Controllers/RolusuariosController.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using vvuelos_backend;
 
 namespace vvuelos_backend.Controllers
 {
+    [EnableCors(origins: "http://localhost:3001 , http://localhost:3000 ", headers: "*", methods: "*")]
     public class RolusuariosController : ApiController
     {
         private vvuelosEntities db = new vvuelosEntities();
