0017d0a59cfc0b6f150763f0ace7417a3bd06332
JSaenzCalvo <josesaenzcalvo@gmail.com>
2020-12-17 00:29:23 -0600

    ReservacionesUsuarios

---
+++ Controllers/ReservacionesUsuariosController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using vvuelos_backend;
+
+namespace vvuelos_backend.Controllers
+{
+    public class ReservacionesUsuariosController : ApiController
+    {
+        private vvuelosEntities db = new vvuelosEntities();
+
+        
+
+        // GET: api/ReservacionesUsuarios/5
+        [ResponseType(typeof(Reservacion))]
+        public IHttpActionResult GetReservacion(string id)
+        {
+            List<Reservacion> reservacion = db.Reservacions.Where(Reservacion => Reservacion.UsuarioID.Equals(id)).ToList();
+            if (reservacion == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(reservacion);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ReservacionExists(string id)
+        {
+            return db.Reservacions.Count(e => e.Consecutivo == id) > 0;
+        }
+    }
+}
